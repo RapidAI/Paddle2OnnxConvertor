6acc319dc5ee90bb0b40f36180a74423b88d4d75
benjaminwan <benjaminwan@163.com>
2021-03-27 20:18:53 +0800

    fix paddle2onnx-wild url

--- download-models.sh
+++ download-models.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-wget https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_train.tar
-wget https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_det_train.tar
+wget -c -i paddle-model-list.txt
 
-tar xf ./ch_ppocr_mobile_v2.0_cls_train.tar
-tar xf ./ch_ppocr_mobile_v2.0_det_train.tar
+function unTarToDir() {
+	mkdir ${1}
+	tar xf ${1}.tar -C ${1} --strip-components 1
+	if [[ "`ls -A ${1}`" = "" ]]; then
+    	tar xf ${1}.tar -C ${1}
+	fi
+}
+
+function unTarFile() {
+	fullName=${1##*/}
+	echo "unTarToDir ${fullName%.*}"
+	unTarToDir ${fullName%.*}
+}
+
+export -f unTarToDir
+export -f unTarFile
+find . -name '*.tar' -type f -exec bash -c 'unTarFile "$0"' {} \;
